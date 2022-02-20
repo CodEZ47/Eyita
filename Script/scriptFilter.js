d883abff2c030405768a4169b1219a655e385fd5
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-02-20 20:12:59 +0300

    some updates

--- Script/scriptFilter.js
+++ Script/scriptFilter.js
@@ -4,11 +4,19 @@ const adv = document.querySelectorAll('.adv')
 const horror = document.querySelectorAll('.horror')
 const drama = document.querySelectorAll('.drama')
 const card = document.querySelectorAll('.card')
+const vl = document.querySelectorAll('.vl')
+const wrap = document.querySelectorAll('.wrapper')
 
 
 function change(e){
     e.style = "visibility: visible; display: flex;";
 }
+
+
+function flex(e){
+    e.style = "width: 90%; justify-content: flex-start;"
+}
+
 
 function changeIn(e){
     e.style = "visibility: hidden; display: none;";
@@ -20,22 +28,27 @@ console.log(selectedvalue);
 switch(selectedvalue){
     case 'action':
         card.forEach(changeIn);
+        vl.forEach(flex);
         action.forEach(change);
         break;
     case 'comedy':
         card.forEach(changeIn);
+        vl.forEach(flex);
         comedy.forEach(change);
         break;   
     case 'adv':
         card.forEach(changeIn);
+        vl.forEach(flex);
         adv.forEach(change);
         break;  
     case 'drama':
         card.forEach(changeIn);
+        vl.forEach(flex);
         drama.forEach(change);
         break;  
     case 'horror':
         card.forEach(changeIn);
+        vl.forEach(flex);
         horror.forEach(change);
         break; 
     case 'all':
